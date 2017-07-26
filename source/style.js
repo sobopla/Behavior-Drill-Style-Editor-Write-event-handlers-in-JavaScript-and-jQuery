f74fd3d77554ffbdc5631c64eecfe6f69a8b0d1e
Catherine Jean <catherinejeanbean@gmail.com>
2017-07-25 21:53:15 -0500

    Add solution

--- source/style.js
+++ source/style.js
@@ -1,4 +1,23 @@
 // Shorthand for document ready
 $(function(){
-  // your code goes here.
+  $("#style_editor").submit(function() {
+  	event.preventDefault();
+  	
+  	var $selector = $( "#selector" ).val();
+  	var $property = $( "#property" ).val();
+  	var $value = $( "#value" ).val();
+
+  	$( $selector ).css( $property, $value );
+    
+  })
 });
+
+// $( $( "#selector").val() ).css( $( "#property" ).val(), $( "#value" ).val());
+
+
+
+//Here we changed the elements to reference by id instead of 'name' 
+//i.e. instead of
+//name = selector
+//input id = selector 
+//This will work if not using ruby??
